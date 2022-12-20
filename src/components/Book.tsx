cccf425fd91819e902c70567282d5f461f7ab4e0
Eric Lake <ericallenlake@gmail.com>
2022-12-20 11:26:45 -0700

    Componentizes book + adds total pages

---
+++ src/components/Book.tsx
@@ -0,0 +1,49 @@
+import { useSession } from "next-auth/react";
+
+export type dbBook = {
+    title: string,
+    author: string,
+    read: boolean,
+    pages: string
+}
+
+
+const Book = ({ book }: {book: dbBook}) => {
+    const onClick = (e) => {
+        console.log(e)
+    }
+  // const getAll = trpc.example.getUsers.useQuery();
+  const { data: sessionData} = useSession()
+  console.log(sessionData)
+
+
+
+  
+
+
+    return (
+
+        <li
+       
+        className={`  flex text-black flex-col border-black w-[30%] boxshadow bg-white justify-between rounded-md hover:ring-4  ring-[#f7e329] p-6 min-w-[350px] ${
+          "border-4"
+        }`}
+      >
+ 
+        <div className="info">
+          <p tabIndex={book.title.length > 45 ? 0 : 1} className={`font-bold text-2xl ${book.title.length > 45 && 'dfn'}`} data-title={book.title}>{book.title.length > 45 ? book.title.slice(0,45) + '...' : book.title }</p>
+          <p className="font-bold mb-5 h-[100px] overflow-y-auto">Author(s):
+          <br></br>
+         {book.author}
+          </p>
+        </div>
+        <div className="flex justify-between">
+          <p className=" font-bold p-2 border-2 rounded-lg w-fit text-center hover:opacity-75 text-white border-black bg-[#140c0d]">{book.pages} pages</p>
+         <button onClick={onClick}>{book.read ? "Completed" : "Complete"}</button>
+        </div> 
+      </li>
+       
+    );
+    }
+
+    export default Book;
--- src/components/Shelf.tsx
+++ src/components/Shelf.tsx
@@ -1,17 +1,43 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import { trpc } from "../utils/trpc";
+import  Book  from "./Book"
+import Navbar from "./Navbar";
+import { useState, useEffect } from "react";
 
 export default function Shelf() {
     const { data: sessionData } = useSession();
-
+  
     const { data: yourShelf } = trpc.example.getUserBooks.useQuery(sessionData?.user?.id || 'nouser');
 
-    return (
+    const [pagesRead, setPagesRead] = useState(0)
+    
+  useEffect(() => {
+    let all = 0
+    yourShelf?.forEach(book => {
+     
+      all += Number(book.pages)
+              })
+    setPagesRead(all)
+    
+  }, [yourShelf])
 
-        <div className='flex flex-col  min-h-screen'>
-          {yourShelf?.map(book => (
-            <h2 key={book.id}>{book.title}</h2>
-          ))}
-        </div>
+
+    return (
+      <>
+      <Navbar />
+      
+        <div className="flex flex-col justify-center items-center">
+    
+            <h1 className="font-extrabold text-5xl m-5">Your Shelf</h1>
+            <div className='text-white flex justify-center mx-auto flex-wrap gap-4 max-w-[1200px] min-h-full'>
+              {yourShelf?.map(book => (
+                <Book key={book.id} book={book} />
+              ))}
+            </div>
+            {
+             } <p className="font-extrabold text-xl m-5">{pagesRead} pages read</p>
+       
+      </div>
+      </>
     );
     }
